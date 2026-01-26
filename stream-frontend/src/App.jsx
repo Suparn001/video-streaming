ada60a1ec1dcf7fe3c6b3896434c5d3d85bc205b
Suparn001 <suparngautam@gmail.com>
2026-01-27 00:58:55 +0530

    started frontend for video uploading

---
+++ stream-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center py-9'>
+        <h1 className='text-5xl font-bold text-gray-700 dark:text-gray-100'>Welcome to Video Streaming application!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
